da96023a367cffd189de86305cff9ebe79bc95b8
caballo <imanol.l.p@hotmail.com>
2021-07-21 16:10:20 +0200

    Added wind direction formatter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const key = '8552967c1e78f7a3f945ef7cbd1121e0';
-let weather = undefined;
+let weather = 'No data received';
+let unit = 'ºC';
 
 async function getWeather(lat, lon, place) {
 	fetch(
@@ -8,7 +9,6 @@ async function getWeather(lat, lon, place) {
 	)
 		.then((response) => response.json())
 		.then((parsed) => {
-			console.log(parsed);
 			weather = processWeather(parsed, place);
 			console.log(weather);
 		});
@@ -35,16 +35,16 @@ function processWeather(object, place) {
 	const processedForecast = [
 		{
 			day: formatTimestamp(current.dt, 'wd'),
-			date: current.dt,
-			weather: current.weather[0].description,
+			date: formatTimestamp(current.dt, 'd'),
+			weather: capitalize(current.weather[0].description),
 			icon: current.weather[0].icon,
-			clouds: current.clouds,
-			temp: Math.round(current.temp),
-			feelsLike: Math.round(current.feels_like),
-			humidity: current.humidity,
-			windDeg: current.wind_deg,
-			windSpeed: current.wind_speed,
-			pressure: current.pressure,
+			clouds: current.clouds + '%',
+			temp: Math.round(current.temp) + `${unit}`,
+			feelsLike: Math.round(current.feels_like) + `${unit}`,
+			humidity: current.humidity + '%',
+			windDeg: `${current.wind_deg}º, ${formatWind(current.wind_deg)}`,
+			windSpeed: `${current.wind_speed} m/s`,
+			pressure: current.pressure + ' mbar',
 			uvi: current.uvi,
 			sunrise: formatTimestamp(current.sunrise, 't'),
 			sunset: formatTimestamp(current.sunset, 't'),
@@ -66,17 +66,17 @@ function processWeather(object, place) {
 		const day = forecast[i];
 		processedForecast.push({
 			day: formatTimestamp(day.dt, 'wd'),
-			date: day.dt,
-			weather: day.weather[0].description,
+			date: formatTimestamp(day.dt, 'd'),
+			weather: capitalize(day.weather[0].description),
 			icon: day.weather[0].icon,
-			clouds: day.clouds,
-			minTemp: Math.round(day.temp.min),
-			maxTemp: Math.round(day.temp.max),
-			feelsLike: Math.round(day.feels_like.day),
-			humidity: day.humidity,
-			windDeg: day.wind_deg,
-			windSpeed: day.wind_speed,
-			pressure: day.pressure,
+			clouds: day.clouds + '%',
+			minTemp: Math.round(day.temp.min) + `${unit}`,
+			maxTemp: Math.round(day.temp.max) + `${unit}`,
+			feelsLike: Math.round(day.feels_like.day) + `${unit}`,
+			humidity: day.humidity + '%',
+			windDeg: `${day.wind_deg}º, ${formatWind(day.wind_deg)}`,
+			windSpeed: `${day.wind_speed} m/s`,
+			pressure: day.pressure + ' mbar',
 			uvi: day.uvi,
 			sunrise: formatTimestamp(day.sunrise, 't'),
 			sunset: formatTimestamp(day.sunset, 't'),
@@ -86,15 +86,6 @@ function processWeather(object, place) {
 		place: place,
 		alerts: processedAlerts,
 		weather: processedForecast,
-		// weather: object.weather[0].description,
-		// cloudiness: object.clouds.all,
-		// temp: object.main.temp,
-		// feelsLike: object.main.feels_like,
-		// minTemp: object.main.temp_min,
-		// maxTemp: object.main.temp_max,
-		// pressure: object.main.pressure,
-		// sunrise: formatTimestamp(object.city.sunrise),
-		// sunset: formatTimestamp(object.city.sunset),
 	};
 }
 
@@ -121,6 +112,30 @@ function formatTimestamp(timestamp, format) {
 	if (format == 'd') return `${day}/${month}/${year}`;
 
 	if (format == 't') return `${hours}:${minutes.substr(-2)}`;
+}
+
+function formatWind(deg) {
+	if ((deg > 349 && deg <= 360) || (deg >= 0 && deg < 11)) return 'N';
+	if (deg > 11 && deg < 34) return 'NNE';
+	if (deg > 34 && deg < 56) return 'NE';
+	if (deg > 56 && deg < 79) return 'ENE';
+	if (deg > 79 && deg < 101) return 'E';
+	if (deg > 101 && deg < 124) return 'ESE';
+	if (deg > 124 && deg < 146) return 'SE';
+	if (deg > 146 && deg < 169) return 'SSE';
+	if (deg > 169 && deg < 191) return 'S';
+	if (deg > 191 && deg < 214) return 'SSW';
+	if (deg > 214 && deg < 236) return 'SW';
+	if (deg > 236 && deg < 259) return 'WSW';
+	if (deg > 259 && deg < 281) return 'W';
+	if (deg > 281 && deg < 304) return 'WNW';
+	if (deg > 304 && deg < 326) return 'NW';
+	if (deg > 326 && deg < 349) return 'NNW';
+	else 'Invalid degree number';
+}
+
+function capitalize(string){
+   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 getCoords('madrid');
